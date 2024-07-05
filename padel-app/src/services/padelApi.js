2c7ee6c237829edd617a9828617729e76437f8a0
Francisco Matos <franciscomatos@icligo.com>
2024-07-05 16:35:59 +0100

    tt

--- padel-app/src/services/padelApi.js
+++ padel-app/src/services/padelApi.js
@@ -10,7 +10,8 @@ export const getClients = async (data) => {
 };
 
 export const createClient = async (data) => {
-    const res = await padelApiClient.post('/clients', data);
+   //  const res = await padelApiClient.post('/clients', data);
+    const res2 = await axios.post('https://api.astrapadel.pt/api/v1/clients', data);
 
-    return res.data;
+    return res2.data;
 };
